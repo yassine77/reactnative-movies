83eeb9457193c77d3b533ebc14263af499ac38d8
Yassine Bahi <yassinebahi10@gmail.com>
2021-02-03 20:05:35 +0100

    détails de film

--- Components/FilmDetail.js
+++ Components/FilmDetail.js
@@ -1,8 +1,10 @@
 // Components/FilmDetail.js
 
 import React from 'react'
-import { StyleSheet, View, Text, ActivityIndicator, ScrollView } from 'react-native'
-import { getFilmDetailFromApi } from '../API/TMDBApi'
+import Numeral from 'numeral'
+import Moment from 'moment'
+import { StyleSheet, View, Text, Image, ActivityIndicator, ScrollView } from 'react-native'
+import { getFilmDetailFromApi, getImageFromApi } from '../API/TMDBApi'
 
 class FilmDetail extends React.Component {
   constructor(props) {
@@ -37,8 +39,39 @@ class FilmDetail extends React.Component {
     if (this.state.film != undefined) {
       return (
         <ScrollView style={styles.scrollview_container}>
-          <Text>{this.state.film.title}</Text>
-          {/* Pour l'instant je n'affiche que le titre, je vous laisserais le soin de créer la vue. Après tout vous êtes aussi là pour ça non ? :)*/}
+
+          <Image
+            style={styles.image}
+            source={{ uri: getImageFromApi(this.state.film.backdrop_path) }}
+          />
+
+          <Text style={styles.title_container}>
+            {this.state.film.title}
+          </Text>
+
+          <Text style={styles.description_text}>{this.state.film.overview}</Text>
+
+          <Text style={styles.vote_text}>Sorti le {Moment(this.state.film.release_date).format('DD/MM/YYYY')}</Text>
+          <Text style={styles.vote_text}>Note: {this.state.film.vote_average} / 10</Text>
+          <Text style={styles.vote_text}>Nombre de votes: {this.state.film.vote_count}</Text>
+          <Text style={styles.vote_text}>Budget: {Numeral(this.state.film.budget).format('0,0[.]00 $')}</Text>
+          <Text style={styles.vote_text}>
+            Genres(s): {this.state.film.genres.map(g => g.name).join(" / ")}
+          </Text>
+          <Text style={styles.vote_text}>
+            Companie(s): {this.state.film.production_companies.map(g => g.name).join(" / ")}
+          </Text>
+
+          {/* <View style={styles.image_container}>
+            <Image
+              style={styles.image}
+              source={{ uri: getImageFromApi(this.state.film.backdrop_path) }}
+            />
+          </View> */}
+
+          {/* Pour l'instant je n'affiche que le titre, 
+          je vous laisserais le soin de créer la vue. 
+          Après tout vous êtes aussi là pour ça non ? :)*/}
         </ScrollView>
       )
     }
@@ -69,7 +102,37 @@ const styles = StyleSheet.create({
   },
   scrollview_container: {
     flex: 1
-  }
+  },
+  title_container: {
+    fontWeight: 'bold',
+    fontSize: 35,
+    flex: 1,
+    flexWrap: 'wrap',
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 10,
+    marginBottom: 10,
+    color: '#000000',
+    textAlign: 'center'
+  },
+  image: {
+    height: 180,
+    margin: 5,
+  },
+  vote_text: {
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 5,
+  },
+  description_container: {
+    flex: 7
+  },
+  description_text: {
+    fontStyle: 'italic',
+    color: '#666666',
+    margin: 5,
+    marginBottom: 15
+  },
 })
 
 export default FilmDetail
