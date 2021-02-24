1f534ce8c85812fc94d9432606805dd53648a281
Yassine Bahi <yassinebahi10@gmail.com>
2021-02-24 18:21:57 +0100

    PanResponder: move with touch

--- Components/Test.js
+++ Components/Test.js
@@ -1,7 +1,7 @@
 // Components/Test.js
 
 import React from 'react'
-import { StyleSheet, View, Animated, Easing } from 'react-native'
+import { StyleSheet, View, Animated, Easing, Dimensions, PanResponder } from 'react-native'
 
 
 class Test extends React.Component {
@@ -9,42 +9,32 @@ class Test extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      topPosition: new Animated.Value(0)
+      topPosition: 0,
+      leftPosition: 0,
     }
-  }
 
-  animate() {
-    Animated.sequence([
-      Animated.spring(
-        this.state.topPosition,
-        {
-          toValue: 100,
-          tension: 8,
-          friction: 3,
-          useNativeDriver: false
+    var { height, width } = Dimensions.get('window');
+    this.panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: (evt, gestureState) => true,
+      onPanResponderMove: (evt, gestureState) => {
+        let touches = evt.nativeEvent.touches;
+        if (touches.length == 1) {
+          this.setState({
+            topPosition: touches[0].pageY - height / 2,
+            leftPosition: touches[0].pageX - width / 2
+          })
         }
-      ),
-      Animated.timing(
-        this.state.topPosition,
-        {
-          toValue: 0,
-          duration: 1000,
-          easing: Easing.elastic(2),
-          useNativeDriver: false
-        }
-      )
-    ]).start()
-  }
-
-  componentDidMount() {
-    this.animate()
+      }
+    })
   }
 
   render() {
     return (
       <View style={styles.main_container}>
-        <Animated.View style={[styles.animation_view, { top: this.state.topPosition }]}>
-        </Animated.View>
+        <View
+          {...this.panResponder.panHandlers}
+          style={[styles.animation_view, { top: this.state.topPosition, left: this.state.leftPosition }]}>
+        </View>
       </View>
     )
   }
@@ -57,7 +47,7 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   animation_view: {
-    backgroundColor: 'red',
+    backgroundColor: 'blue',
     width: 100,
     height: 100
   }
